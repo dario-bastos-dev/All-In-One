60f278bd7d4f24b0badc9848f3913d0de00cb10f
Dario Bastos <dario.bastos.dev@gmail.com>
2024-11-06 19:56:06 -0300

    Modificando cors

--- server.ts
+++ server.ts
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "http://http://127.0.0.1:5500",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
