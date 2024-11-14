9efbbb3213d8d5ec404ce47f2d8a164a30c69403
Dario Bastos <dario.bastos.dev@gmail.com>
2024-11-14 12:35:37 -0300

    Modify cors

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 const corsConfig = cors({
-  origin: "127.0.0.1:5500",
+  origin: "http://127.0.0.1:5500",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 });
